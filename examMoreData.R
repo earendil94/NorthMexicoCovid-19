40a6b09f4a45e120f67463a4a4dc89d9b8e904ec
nicolas solomita <solonicolas@MBP-di-nicolas.homenet.telecomitalia.it>
2020-07-23 10:32:24 +0200

    minor fix

--- examMoreData.R
+++ examMoreData.R
@@ -11,36 +11,39 @@ library(Metrics)
 ##### CSV loading #####
 data <- read.csv("MexicoCovid19Updated.csv", header = T, sep = ",")
 data$Date <- as.Date(data$Date,"%Y-%m-%d")
-
-#A random 120 year old person appears, gratz mate!
-max(data$Age)
+population = read.csv("./population.csv",header=T)
+names = c("BAJA CALIFORNIA","CHIHUAHUA","COAHUILA","DURANGO","NUEVO LEÓN",
+          "SINALOA","SONORA","TAMAULIPAS","ZACATECAS")
 
 for (i in names){
   data$population[data$Region==i] <- population[population$Estado==i,2]
 }
 
+# A random 120 year old person appears, gratz mate!
+max(data$Age)
+
 ##### Preliminary plotting #####
 # hist sex
 plot(data$Sex)
 
-#hist age 0-20 20-30 30-40 40-50 50-60 60-70 70-80 80+
+# hist age 0-20 20-30 30-40 40-50 50-60 60-70 70-80 80+
 data$age_class <- factor(data$Age%/%10)
 plot(data$age_class)
 
-#cumulative
+# cumulative
 data$time <- data$Date-data$Date[1]
 plot(data$time,rownames(data),type="l")
 
 # by regions
-plot(data$Region)
+plot(data$Region, las=3)
 
-#Daily data
+# Daily data
 data_daily <- data %>%
   group_by(time) %>%
   summarize(daily_cases = n())
 
 
-##### Adding lockdown and post_lockdown columns #####
+# Adding lockdown and post_lockdown columns
 first_day_data <- min(data$Date)
 lockdown_start <- as.Date("2020-03-23")
 lockdown_end <- as.Date("2020-06-01")
@@ -57,38 +60,44 @@ post_lock <- seq(1,as.integer(last_day_data - lockdown_end)+1)
 post_lockdown <- c(no_lock, post_lock)
 data_daily$post_lockdown <- post_lockdown
 
-#Daily data show a strong periodicity due to (possibly) sunday testing?
+# Daily data show a strong periodicity due to (possibly) sunday testing?
 plot(data_daily$time, data_daily$daily_cases, type = "l")
 
-#Moving averages make them smooth
-#View(data_daily)
+# Moving averages make them smooth
 data_daily$avg_cases <- movavg(data_daily$daily_cases, n= 7, type="s")
 data_daily$avg_cases <- as.integer(data_daily$avg_cases)
 data_daily$time <- as.integer(data_daily$time)
 plot(data_daily$time, data_daily$avg_cases, type = "l")
 
 ##### Train/Test split #####
-
 test_days = 5
 days <- as.integer(data_daily$time[length(data_daily$time)])
 training_set <- data_daily[data_daily$time<=days-test_days,]
 test_set <- data_daily[data_daily$time>days-test_days,]
 
 
-##### Poisson regression #####
+##### Plot and Loss functions #####
 
-loss <- function(model, test){
+# bayesian
+loss_stan <- function(model, test){
   pred_model <- posterior_predict(model, newdata = test)
   pred_0025 <- apply(pred_model, 2, function(x) quantile(x, 0.025))
   pred_0975 <- apply(pred_model, 2, function(x) quantile(x, 0.975))
-  pred_model <- apply(pred_model, 2, mean)
+  pred_mean <- apply(pred_model, 2, mean)
   plot(test$time, test$avg_cases, type="l")
-  points(test$time, pred_model, type="l", col="red")
+  points(test$time, pred_mean, type="l", col="red")
   lines(test$time, pred_0025, lty=2, col="red")
   lines(test$time, pred_0975, lty=2, col="red")
-  return(mse(test$avg_cases, pred_model))
+  return(mse(test$avg_cases, pred_mean))
 }
 
+# frequentist
+loss <- function(model, test){
+  pred <- predict(model, newdata = test, type = "response")
+  plot(test$time, test$avg_cases, type="l")
+  points(test$time, pred, type="l", col="red")
+  return(mse(test$avg_cases, pred))
+}
 
 #TODO: NEED TO FIGURE THIS OUT
 #?loo
@@ -96,39 +105,25 @@ loss <- function(model, test){
 #loo_model_time_2 <- loo(model_time_2)
 #loo_model_time_2$estimates[3,1] #This yields the LOOIC: the lowest the better
 
-#Plot of the fit in red
-#plot(data_daily$time, data_daily$avg_cases, type = "l")
-#points(data_daily$time, model_time_2$fitted.values, type = "l", col = "red")
-
-#CI
-#model_time_2$stan_summary
-
-
 plot(data_daily$time, data_daily$avg_cases, type = "l")
 abline(v=c(82, 152), lty=2)
 
+##### Poisson Regression #####
+
 # time
 model_time <- stan_glm(avg_cases ~ time, family = poisson,  data=training_set)
-loss(model_time, test_set)
+loss_stan(model_time, test_set)
 
 # time^2
 model_time_2 <- stan_glm( avg_cases ~ time + I(time^2), family = poisson,  data=training_set)
-loss(model_time_2, test_set)
-model_time_2$coefficients
+loss_stan(model_time_2, test_set)
 
 # time^3
 model_time_3 <- stan_glm(avg_cases ~ time + I(time^2) + I(time^3), family = poisson,  data=training_set)
-loss(model_time_3, test_set)
+loss_stan(model_time_3, test_set)
 
-# exp(time) !!!!!!!!!!!!!!!!!!
-# 
-?stan_glm
-model_time_exp <- stan_glm(avg_cases ~ time + I(time^2) + I(exp(time)), family = poisson,  data=training_set)
-mse(test_set$avg_cases, predict.glm(model_time_exp, newdata = test_set, type = "response"))
-test_set$avg_cases
-plot(training_set$time, training_set$avg_cases, type="l")
-points(training_set$time, model_time_exp$fitted.values, type="l", col="red")
-points(test_set$time, predict(model_time_exp, newdata = test_set, type="response"), type="l", col="red")
+# exp(time) !!!!!!
+model_time_exp <- glm(avg_cases ~ time + I(time^2) + I(exp(time)), family = poisson,  data=training_set)
 loss(model_time_exp, test_set)
 
 # lockdown
@@ -140,7 +135,6 @@ loss(model_time_lock, test_set)
 ################################################################################
 ###################### DIVISION ################################################
 ################################################################################
-
 
 # SEX
 
@@ -211,7 +205,7 @@ Zacatecas <- data %>%
   filter(Region=="ZACATECAS")
 
 # plots
-plot(BajaCalifornia$time, BajaCalifornia$daily_cases, type = "l", ylim = c(0,1000))
+plot(BajaCalifornia$time, BajaCalifornia$daily_cases, xlab="time", ylab="cases", type = "l", ylim = c(0,1000))
 points(Chihuahua$time, Chihuahua$daily_cases, type="l", col=2)
 points(Coahuila$time, Coahuila$daily_cases, type="l", col=3)
 points(Durango$time, Durango$daily_cases, type="l", col=4)
@@ -233,7 +227,7 @@ Tamaulipas$avg_cases <- movavg(Tamaulipas$daily_cases, n= 7, type="s")
 Zacatecas$avg_cases <- movavg(Zacatecas$daily_cases, n= 7, type="s")
 
 # smooth plots
-plot(BajaCalifornia$time, BajaCalifornia$avg_cases, type = "l", ylim = c(0,800))
+plot(BajaCalifornia$time, BajaCalifornia$avg_cases, xlab="time", ylab="cases", type = "l", ylim = c(0,800))
 points(Chihuahua$time, Chihuahua$avg_cases, type="l", col=2)
 points(Coahuila$time, Coahuila$avg_cases, type="l", col=3)
 points(Durango$time, Durango$avg_cases, type="l", col=4)
@@ -267,7 +261,7 @@ old <-data %>%
   summarize(daily_cases = sum(daily_cases))
 
 # plots
-plot(young$time, young$daily_cases, type = "l", ylim = c(0,2500))
+plot(young$time, young$daily_cases, xlab="time", ylab="cases", type = "l", ylim = c(0,2500))
 points(middle_age$time, middle_age$daily_cases, type="l", col=2)
 points(old$time, old$daily_cases, type="l", col=3)
 
@@ -277,7 +271,7 @@ middle_age$avg_cases <- movavg(middle_age$daily_cases, n= 7, type="s")
 old$avg_cases <- movavg(old$daily_cases, n= 7, type="s")
 
 # smooth plots
-plot(young$time, young$avg_cases, type = "l", ylim = c(0,2100))
+plot(young$time, young$avg_cases, xlab="time", ylab="cases", type = "l", ylim = c(0,2100))
 points(middle_age$time, middle_age$avg_cases, type="l", col=2)
 points(old$time, old$avg_cases, type="l", col=3)
 
@@ -299,40 +293,38 @@ plot(data_ts,type="l")
 
 #2 ways to remove trend : subtract or differentiate
 #1
-detrend1 <- data_ts - mod1$fitted.values[7:199]
+detrend1 <- data_ts - model_time_2$fitted.values[7:199]
 par(mfrow=c(1,2))
 plot(detrend1,type="l")
+
 #2
 detrend2 <- diff(data_ts[7:199],diff=2)
 plot(detrend2,type="l")
 par(mfrow=c(1,1))
 
 # now to choose the model look at acf and pacf
-acf(detrend1)
-pacf(detrend1)
+acf(detrend2)
+pacf(detrend2)
 # seems an AR(2) -> ARIMA(2,1,0)
 
 #but trying to use auto selection
 #model <- auto.arima(data_ts,trace=TRUE)
 #summary(model)
 
-model <- arima(data_ts,order=c(2,1,0))
+model <- arima(data_ts, order=c(2,1,0))
 
 #predictions
 predictions = forecast(model, n=30, level = c(80, 95))
 plot(predictions)
 
 
-############################
-### LOGISTIC OR GOMPERTZ ###
-###  ON CUMULATIVE DATA  ###
-############################
+##### LOGISTIC OR GOMPERTZ ON CUMULATIVE DATA  #####
 
 data_daily$cumulative = cumsum(data_daily$daily_cases)
 plot(data_daily$cumulative,type="l")
 
-#fit a logistic function using non linear least squares approximation
-logistic_model <- nls(cumulative~ SSlogis(time, Asym, xmid, scal), data=data_daily)
+# fit a logistic function using non linear least squares approximation
+logistic_model <- nls(cumulative ~ SSlogis(time, Asym, xmid, scal), data=data_daily)
 coeff <- coef(logistic_model)
 x <- 1:400
 plot(x,SSlogis(x,coeff[1],coeff[2],coeff[3]),type="l")
@@ -345,9 +337,8 @@ gomp <- function(data,alpha,beta,k){
 plot(x,gomp(x,300000,188,0.03),type="l")
 points(data_daily$time,data_daily$cumulative,col=2,pch=20)
 
-gomp_model <- nls(cumulative~ SSgompertz(time,alpha, beta, k), data=data_daily)
+gomp_model <- nls(cumulative ~ SSgompertz(time,alpha, beta, k), data=data_daily)
 coeff <- coef(gomp_model)
 coeff 
 plot(x,SSgompertz(x,coeff[1],coeff[2],coeff[3]),type="l")
 points(data_daily$time,data_daily$cumulative,col=2,pch=20)
-
